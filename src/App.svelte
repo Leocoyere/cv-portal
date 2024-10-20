1e490a2792d8e2396bbd9f5a59b37eaf5651e65a
leo <leo.coyere94@gmail.com>
2024-10-20 14:48:44 +0200

    Change color palette + underlined links

--- src/App.svelte
+++ src/App.svelte
@@ -15,10 +15,10 @@
 	  <!-- Title Section -->
 	  <header class="mb-3 p-6">
 		
-		<h1 class="text-4xl font-bold text-indigo-600">Welcome! 🇬🇧</h1>
-		<h1 class="text-4xl font-bold text-teal-500">Bienvenue&nbsp;! 🇫🇷</h1>
-		<p class="mt-3 text-sm text-gray-500">🇬🇧 You speak english?<br> Focus on the <span class="text-indigo-600">indigo</span></p>
-		<p class="mt-1 text-sm text-gray-500">🇫🇷 Vous parlez français&nbsp;?<br> Concentrez vous sur le <span class="text-teal-500">turquoise</span></p>
+		<h1 class="text-4xl font-bold text-blue-600">Welcome! 🇬🇧</h1>
+		<h1 class="text-4xl font-bold text-pink-500">Bienvenue&nbsp;! 🇫🇷</h1>
+		<p class="mt-3 text-sm text-gray-500">🇬🇧 You speak english?<br> Focus on the <span class="text-blue-600">blue</span></p>
+		<p class="mt-1 text-sm text-gray-500">🇫🇷 Vous parlez français&nbsp;?<br> Concentrez vous sur le <span class="text-pink-500">rose</span></p>
 
 		<a class="mail-link" href="mailto:leo.coyere.pro@gmail.com">
 			<svg viewBox="0 0 52 39" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -31,13 +31,13 @@
 	  </header>
 	  
 	  <section class="space-y-4 my-4">
-		<p class="leading-relaxed text-gray-700 bg-indigo-600 bg-opacity-20 p-4 border-l-4 border-indigo-600">
+		<p class="leading-relaxed text-gray-700 bg-blue-600 bg-opacity-20 p-4 border-l-4 border-blue-600">
 			If you're here, it means my QR code caught your attention, and I'm glad! First of all, I hope you're enjoying your time at Disneyland Paris.<br>
 			<!-- <strong>Since this is the place where dreams come true, maybe mine can too?</strong><br> -->
 			I'm a creative person, and I thought this QR code would be a great way to promote my search for a cybersecurity apprenticeship. If you're working in tech or know someone who is, feel free to share this link with your coworkers or friends—it would really help me a lot.<br>
 			You can learn more about me by scrolling down.
 		</p>
-		<p class="leading-relaxed text-gray-700 bg-teal-500 bg-opacity-20 p-4 border-l-4 border-teal-500">
+		<p class="leading-relaxed text-gray-700 bg-pink-500 bg-opacity-20 p-4 border-l-4 border-pink-500">
 			Si vous êtes ici, c'est que mon QR code a attiré votre attention, et j'en suis ravi&nbsp;! Avant tout, j'espère que vous profitez de votre visite à Disneyland Paris.<br>
 			<!-- <strong>Puisque c'est le lieu où les rêves deviennent réalité, peut-être que le mien le deviendra aussi&nbsp;?</strong><br> -->
 			Je suis une personne créative, et je me suis dit que ce QR code serait une bonne façon de promouvoir ma recherche d'alternance en cybersécurité. Si vous travaillez dans le secteur de la technologie ou connaissez quelqu'un qui y est, n'hésitez pas à partager ce lien avec vos collègues ou amis, cela m'aiderait énormément.<br>
@@ -49,44 +49,44 @@
   
 		<!-- Table of Contents Section -->
 		<section class="p-6">
-		  <h2 class="text-2xl font-bold text-indigo-600">Table of Contents 🇬🇧</h2>
-		  <h2 class="text-2xl font-bold text-teal-500">Sommaire 🇫🇷</h2>
+		  <h2 class="text-2xl font-bold text-blue-600">Table of Contents 🇬🇧</h2>
+		  <h2 class="text-2xl font-bold text-pink-500">Sommaire 🇫🇷</h2>
 		  
 		  <ul class="mt-4 space-y-3">
 			<!-- English section links -->
 			<li>
-			  <a href="#about-me" class="text-indigo-600 font-medium hover:underline">
+			  <a href="#about-me" class="text-blue-600 font-medium hover:underline">
 				I - About me
 			  </a>
 			  <br>
-			  <a href="#a-propos-de-moi" class="text-teal-500 font-medium hover:underline">
+			  <a href="#a-propos-de-moi" class="text-pink-500 font-medium hover:underline">
 				I - À propos de moi
 			  </a>
 			</li>
 			<li>
-			  <a href="#what-am-i-looking-for" class="text-indigo-600 font-medium hover:underline">
+			  <a href="#what-am-i-looking-for" class="text-blue-600 font-medium hover:underline">
 				II - What am I looking for?
 			  </a>
 			  <br>
-			  <a href="#qu-est-ce-que-je-recherche" class="text-teal-500 font-medium hover:underline">
+			  <a href="#qu-est-ce-que-je-recherche" class="text-pink-500 font-medium hover:underline">
 				II - Qu'est-ce que je recherche&nbsp;?
 			  </a>
 			</li>
 			<li>
-			  <a href="#why-choose-me" class="text-indigo-600 font-medium hover:underline">
+			  <a href="#why-choose-me" class="text-blue-600 font-medium hover:underline">
 				III - Why choose me?
 			  </a>
 			  <br>
-			  <a href="#pourquoi-me-choisir" class="text-teal-500 font-medium hover:underline">
+			  <a href="#pourquoi-me-choisir" class="text-pink-500 font-medium hover:underline">
 				III - Pourquoi me choisir&nbsp;?
 			  </a>
 			</li>
 			<li>
-			  <a href="#learn-more" class="text-indigo-600 font-medium hover:underline">
+			  <a href="#learn-more" class="text-blue-600 font-medium hover:underline">
 				IV - Learn more
 			  </a>
 			  <br>
-			  <a href="#en-savoir-plus" class="text-teal-500 font-medium hover:underline">
+			  <a href="#en-savoir-plus" class="text-pink-500 font-medium hover:underline">
 				IV - En savoir plus
 			  </a>
 			</li>
@@ -96,14 +96,14 @@
 
 	  <!-- Text Content -->
 	  <section class="space-y-4 my-8 p-6">
-		<h2 class="text-2xl text-indigo-400 font-semibold" id="about-me">
+		<h2 class="text-2xl text-blue-400 font-semibold" id="about-me">
 			I - About me
 		</h2>
 		<p class="leading-relaxed text-gray-700">
 			For three years, I studied web development during my bachelor’s program, where I gained a variety of skills, including learning several programming languages, developing websites and mobile applications, and understanding UX/UI design and its challenges. I also deepened my knowledge in project management. These skills allowed me to work with frameworks and APIs to create efficient digital solutions tailored to user needs.
 			Now, I’m focusing on cybersecurity. For the past year, I’ve been pursuing a Master’s degree specializing in cybersecurity, where I’ve gained expertise in systems and networks, cryptography, and infrastructure and data security. I’ve also explored governance, risk analysis, and key regulatory frameworks.
 		</p>
-		<h2 class="text-2xl text-teal-400 font-semibold" id="a-propos-de-moi">
+		<h2 class="text-2xl text-pink-400 font-semibold" id="a-propos-de-moi">
 			I - À propos de moi
 		</h2>
 		<p class="leading-relaxed text-gray-700">
@@ -113,7 +113,7 @@
 	  </section>
 
 	  <section class="space-y-4 my-8 p-6">
-		<h2 class="text-2xl text-indigo-400 font-semibold" id="what-am-i-looking-for">
+		<h2 class="text-2xl text-blue-400 font-semibold" id="what-am-i-looking-for">
 			II - What am I looking for?
 		  </h2>
 		<p class="leading-relaxed text-gray-700">
@@ -121,7 +121,7 @@
 
 			I am available to work on-site in the Île-de-France region or remotely, including opportunities outside of France. However, if I do not secure an apprenticeship by the end of November, I will be forced to leave my program. Therefore, I am also open to full-time positions (CDI) in the field of cybersecurity.
 		</p>
-		<h2 class="text-2xl text-teal-400 font-semibold" id="qu-est-ce-que-je-recherche">
+		<h2 class="text-2xl text-pink-400 font-semibold" id="qu-est-ce-que-je-recherche">
 			II - Qu'est ce que je recherche&nbsp;?
 		</h2>
 		<p class="leading-relaxed text-gray-700">
@@ -132,7 +132,7 @@
 	  </section>
 
 	  <section class="space-y-4 my-8 p-6">
-		<h2 class="text-2xl text-indigo-400 font-semibold" id="why-choose-me">
+		<h2 class="text-2xl text-blue-400 font-semibold" id="why-choose-me">
 			III - Why choose me?
 		  </h2>
 		<p class="leading-relaxed text-gray-700">
@@ -141,7 +141,7 @@
 			If you're looking for an apprentice or a full-time employee, I might be the ideal candidate, and besides, we both love Disney&nbsp;!<br>
 			<i class="text-gray-500">(Unless your kids dragged you here, of course.)</i>
 		</p>
-		<h2 class="text-2xl text-teal-400 font-semibold" id="pourquoi-me-choisir">
+		<h2 class="text-2xl text-pink-400 font-semibold" id="pourquoi-me-choisir">
 			III - Pourquoi me choisir&nbsp;?
 		</h2>
 		<p class="leading-relaxed text-gray-700">
@@ -152,20 +152,20 @@
 	  </section>
 
 	  <section class="space-y-4 my-8 p-6">
-		<h2 class="text-2xl text-indigo-400 font-semibold" id="learn-more">
+		<h2 class="text-2xl text-blue-400 font-semibold" id="learn-more">
 			IV - Learn more
 		  </h2>
 		<p class="leading-relaxed text-gray-700">
 			I'm always looking to expand my network, and I firmly believe that opportunities are created through connections. So, if you can’t help me directly but are in the tech industry, let’s connect on <a href="https://www.linkedin.com/in/leocoyere">LinkedIn</a>!
 		</p>
-		<p class="text-gray-700">Feel free to reach me at :<br><a href="mailto:leo.coyere.pro@gmail.com" class="text-indigo-600 font-semibold">leo.coyere.pro@gmail.com</a>.</p>
-		<h2 class="text-2xl text-teal-400 font-semibold" id="en-savoir-plus">
+		<p class="text-gray-700">Feel free to reach me at :<br><a href="mailto:leo.coyere.pro@gmail.com" class="text-blue-600 font-semibold">leo.coyere.pro@gmail.com</a>.</p>
+		<h2 class="text-2xl text-pink-400 font-semibold" id="en-savoir-plus">
 			IV - En savoir plus
 		</h2>
 		<p class="leading-relaxed text-gray-700">
 			Je cherche toujours à étendre mon réseau, et je suis convaincu que c’est à travers les rencontres que se créent les opportunités. Donc, si vous ne pouvez pas m’aider directement, mais que vous travaillez dans le milieu de la tech, connectons-nous sur <a href="https://www.linkedin.com/in/leocoyere">LinkedIn</a>&nbsp;!
 		</p>
-		<p class="text-gray-700">N'hésitez pas à me contacter à :<br><a href="mailto:leo.coyere.pro@gmail.com" class="text-teal-500 font-semibold">leo.coyere.pro@gmail.com</a>.</p>
+		<p class="text-gray-700">N'hésitez pas à me contacter à :<br><a href="mailto:leo.coyere.pro@gmail.com" class="text-pink-500 font-semibold">leo.coyere.pro@gmail.com</a>.</p>
 	  </section>
   
 	</div>
